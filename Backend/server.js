d9b72360533fe7eb3129dcb6b3e6a3af0ab827d6
himanshu01010 <himanshubehera08052003@gmail.com>
2024-09-07 15:18:48 +0530

    api link updated

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://microfinanceadmin.markletechandmedia.com","https://microfinance.markletechandmedia.com"],
     credentials:true
 }))
 
